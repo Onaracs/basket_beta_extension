38502e13723d0e5eec4da699b3ec73626926bc74
Onaracs <brendan.scarano@gmail.com>
2014-08-08 22:27:54 -0400

    Extension now able to recieve the tab's url of when it's clicked Removed a lot of unneccessary test code that I was playing around with Still working on accessing the basket site and getting a users baskets

--- popup.js
+++ popup.js
@@ -1,12 +1,22 @@
-// if (localStorage.accessToken) {
-//     var graphUrl = "https://graph.facebook.com/me?" + localStorage.accessToken + "&callback=displayUser";
-//     console.log(graphUrl);
-
-//     var script = document.createElement("script");
-//     script.src = graphUrl;
-//     document.body.appendChild(script);
-
-//     function displayUser(user) {
-//         console.log(user);
-//     }
+// window.onload = function() {
+//     console.log("this is loaded")
+//     $('#popup').on('click', function() {
+//         console.log('This is clicked');
+//         // req = new XMLHttpRequest();
+//         // test = req.open('GET', 'http://http://localhost:3000/folders');
+//         // console.log(test);
+//     })
 // }
+  
+//returns the current URL
+window.onload = function() {
+    chrome.tabs.getSelected(null, function(tab) {
+        console.log(tab);
+        tabUrl = tab.url;
+        $('#popup').html(tabUrl);
+    });
+
+    req = new XMLHttpRequest();
+    test = req.open('GET', 'http://localhost:3000/folders', true);
+    $('#test-info').html(test);
+}
