f755137179ab39f048d3432349e357893201ecaa
Onaracs <brendan.scarano@gmail.com>
2014-08-19 13:55:46 -0400

    Extension now lists a users baskets, and once a basket is clicked on a webpage, the url will save to that specific basket

--- background.js
+++ background.js
@@ -4,12 +4,3 @@
 //   chrome.browserAction.setBadgeText({text: ''});
 //   bg = chrome.extension.getBackgroundPage();
 // }
-
-    $(document).ready(function(){
-        $('#popup').on('click', function() {
-            console.log('This is clicked');
-            // req = new XMLHttpRequest();
-            // test = req.open('GET', 'http://http://localhost:3000/folders');
-            // console.log(test);
-        })
-    })
