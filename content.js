490395e27cd5e49739f9a6bf1e44b8417961ccc6
Onaracs <brendan.scarano@gmail.com>
2014-09-04 12:15:36 -0400

    Popup.js now displays both the users baskets and friends, and has the jquery working properly Need to implement the send data function next

--- content.js
+++ content.js
@@ -5,13 +5,16 @@ chrome.runtime.onMessage.addListener(
     var metas = document.getElementsByTagName('meta');
 
     for (i=0; i < metas.length; i++){
-      if (metas[i].name === "og:title" || metas[i].getAttribute("property") === "og:title"){
+      if (metas[i].name === "og:title" || 
+          metas[i].getAttribute("property") === "og:title"){
         info["title"] = metas[i].content;
       } 
-      else if (metas[i].name === "og:description" || metas[i].getAttribute("property") === "og:description"){
+      else if (metas[i].name === "og:description" || 
+                metas[i].getAttribute("property") === "og:description"){
         info["description"] = metas[i].content;
       } 
-      else if (metas[i].name === "og:image" || metas[i].getAttribute("property") === "og:image"){
+      else if (metas[i].name === "og:image" || 
+                metas[i].getAttribute("property") === "og:image"){
         info["image"] = metas[i].content;
       } 
     };
@@ -21,14 +24,7 @@ chrome.runtime.onMessage.addListener(
       info["title"] = title;
     };
 
-    if (request.ping == "Send Page Info")
+    if (request.ping == "Send Page Info"){
       sendResponse({page_info: info});
-  });
-
-    // var question = Object.keys(title[0]);
-    // var question = title[0].text;
-    
-    // var question = Object.keys(metas);
-    // var question = Object.keys(title);
-    // var question = document.querySelector("meta[name='og:title']").attr('content');
-    // var question = document.getElementsbyTagName('meta').item(property='og:title');
+    }
+});
--- popupTest.js
+++ popupTest.js
@@ -1,49 +1,38 @@
-window.onload = function() {
-  chrome.tabs.getSelected(null, function(tab) {
-      tabUrl = tab.url;
-  });
+//Old Code
+//================================
 
-  request_handler('http://localhost:3000/users_folders', '.basket-list')
-  request_handler('http://localhost:3000/users_friends', '.friend-list')
 
-  $('.basket-list').on('click', 'basket-click', function() {
-    sendData('new_link', tabUrl)
-  }
+    // $('.basket-list').on('click', 'basket-click', function() {
+    //   sendData('new_link', tabUrl)
+    // }
 
-  $('.friend-list-wrapper').on('click', 'friend-container', function() {
-    sendData('sent_link', tabUrl)
-  }
-}
+    // $('.friend-list-wrapper').on('click', 'friend-container', function() {
+    //   sendData('sent_link', tabUrl)
+    // }
 
-function request_handler(url, changed_div) {
-  var req = new XMLHttpRequest();
-  req.open('GET', url, true);
-  req.onreadystatechange = function(){
-      // successful response
-      if (req.readyState==4 && req.status==200) {
-          // console.log(req.responseText);
-          document.querySelector(changed_div).innerHTML = (req.responseText);
-      }
-  }
-  req.send();
-}
+    // $('.basket-list').on('click', '.basket-click', function(){
+    //     var id = $(this).attr('value');
+    //     var req = new XMLHttpRequest(); 
+    //     req.open("POST", 'http://localhost:3000/new_link');
+    //     req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    //     req.send(JSON.stringify({url: tabUrl,
+    //                             basketId: id}));
+    // })
 
-function sendData(path, tabUrl){
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-    chrome.tabs.sendMessage(tabs[0].id, {ping: "Send Page Info"}, function(response) {
-      console.log(response.page_info);
-      var pageInfo = response.page_info;
-    });  
-  });
+    // $('.friend-list-wrapper').on('click', '.friend-container', function() {
+    //     var id = $(this).attr('value');
+    //     var req = new XMLHttpRequest(); 
+    //     req.open("POST", 'http://localhost:3000/sent_link');
+    //     req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    //     req.send(JSON.stringify({url: tabUrl,
+    //                             friendId: id}));
+    // })
 
-  var id = $(this).attr('value');
-  var req = new XMLHttpRequest(); 
-
-  req.open("POST", 'http://localhost:3000/' + path);
-  req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-  req.send(JSON.stringify({url: tabUrl,
-                          friendId: id,
-                          pageTitle: pageInfo.title,
-                          pageDescription: pageInfo.description,
-                          pageImg: pageInfo.image}));
-}
+    // $('#click').on('click', function () {
+    //     // chrome.tabs.executeScript(null, { "code": "extension.highlight_h1()" });
+    //     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    //         chrome.tabs.sendMessage(tabs[0].id, {ping: "Send Page Info"}, function(response) {
+    //                 console.log(response.page_info);
+    //         });  
+    //     });
+    // });
